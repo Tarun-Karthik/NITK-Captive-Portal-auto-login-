190e005c08f31e190e5328143cdda8b8782bbded
Karthik <tarunkarthik999@gmail.com>
2018-03-04 19:53:13 +0000

    uploaded the .json and .js files

---
+++ login.js
@@ -0,0 +1,8 @@
+'use strict';
+
+setTimeout(() => {
+document.getElementById("username").value = "username";
+document.getElementById("password").value = "password"
+
+document.getElementById("loginbutton").click();
+}, 2000)
